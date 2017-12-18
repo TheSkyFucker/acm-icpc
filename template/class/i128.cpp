9fce047fe151e041b6dff204a4a53ad462112baf
TheSkyFucker <359084415@qq.com>
2017-12-18 23:27:48 +0800

    template(class/i128) updated.

--- template/class/i128.cpp
+++ template/class/i128.cpp
@@ -4,10 +4,8 @@ struct i128 {
 	static const ll INF = 1e18;
 	ll h, l;
 	i128() {} i128(ll _h, ll _l) : h(_h), l(_l) { //h, l: [-1e18 + 1, 1e18)
-		if (l >= INF) { l -= INF; h++; }
-		else if (l <= -INF) { l += INF; h--; }
-		else if (h > 0 && l < 0) { l += INF; h--; }
-		else if (h < 0 && l > 0) { l -= INF; h++; }
+		if (l >= INF || (h < 0 && l > 0)) { l -= INF; h++; }
+		else if (l <= -INF || (h > 0 && l < 0)) { l += INF; h--; }
 	}
 	i128 operator +(const i128 b) { return i128(h + b.h, l + b.l); } //b: [-1e36, 1e36]
 	i128 operator -(const i128 b) { return i128(h - b.h, l - b.l); } //b: [-1e36, 1e36]
