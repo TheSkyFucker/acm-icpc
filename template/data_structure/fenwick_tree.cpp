e495ffeb60c4dfe13f2adfe418df969c8e4c7901
TheSkyFucker <359084415@qq.com>
2017-12-19 17:18:50 +0800

    template<data_structure/fenwick> fixed.

--- template/data_structure/fenwick_tree.cpp
+++ template/data_structure/fenwick_tree.cpp
@@ -7,11 +7,11 @@ struct Fenwick {
 	ll a1[N], a2[N]; int n;
 	void init(int _n) { n = _n; }
 	void add(ll *a, int p, ll v) { for (; p <= n; p += p & -p) a[p] += v; }
-	void add(int l, int r, ll v) {
+	void add(int l, int r, ll v) { //[l, r]
 		add(a1, l, v), add(a1, r + 1, -v);
 		add(a2, l, -v * (l - 1)), add(a2, r + 1, v * r);
 	}
 	ll sum(ll *a, int p) { ll r = 0; for (; p > 0; p -= p & -p) r += a[p]; return r; }
 	ll pre(int p) { return !p ? 0 : sum(a1, p) * p + sum(a2, p); }
-	ll sum(int l, int r) { return pre(r) - pre(l - 1); }
+	ll sum(int l, int r) { return pre(r) - pre(l - 1); } //[l, r]
 }	fen;
