5ff7eb8de8cd9204d949614af8e649ca7bc365a1
TheSkyFucker <359084415@qq.com>
2017-12-19 17:13:25 +0800

    add template(data_structure/fenwick).

---
+++ template/data_structure/fenwick_tree.cpp
@@ -0,0 +1,17 @@
+// index : [1, n]
+// time : nlogn
+// support : segment add, sum
+// !!!! : use before init()!
+struct Fenwick {
+	static const int N = 2e5 + 7;
+	ll a1[N], a2[N]; int n;
+	void init(int _n) { n = _n; }
+	void add(ll *a, int p, ll v) { for (; p <= n; p += p & -p) a[p] += v; }
+	void add(int l, int r, ll v) {
+		add(a1, l, v), add(a1, r + 1, -v);
+		add(a2, l, -v * (l - 1)), add(a2, r + 1, v * r);
+	}
+	ll sum(ll *a, int p) { ll r = 0; for (; p > 0; p -= p & -p) r += a[p]; return r; }
+	ll pre(int p) { return !p ? 0 : sum(a1, p) * p + sum(a2, p); }
+	ll sum(int l, int r) { return pre(r) - pre(l - 1); }
+}	fen;
