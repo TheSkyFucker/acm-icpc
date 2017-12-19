115177545290a67f1aa51eb1808a924bbff1b42a
TheSkyFucker <359084415@qq.com>
2017-12-19 19:05:15 +0800

    template<data_structure/fenwick_tree> fixed.

--- template/data_structure/fenwick_tree.cpp
+++ template/data_structure/fenwick_tree.cpp
@@ -5,7 +5,11 @@
 struct Fenwick {
 	static const int N = 2e5 + 7;
 	ll a1[N], a2[N]; int n;
-	void init(int _n) { n = _n; }
+	void init(int _n) { 
+		n = _n;
+		fill_n(a1 + 1, n, 0);
+		fill_n(a2 + 1, n, 0);
+	}
 	void add(ll *a, int p, ll v) { for (; p <= n; p += p & -p) a[p] += v; }
 	void add(int l, int r, ll v) { //[l, r]
 		add(a1, l, v), add(a1, r + 1, -v);
