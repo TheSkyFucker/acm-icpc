19cb89c1201bd9c6835430ef384f9827e8323f67
kino <359084415@qq.com>
2018-01-02 22:52:46 +0800

    add template<base/vimrc>.

---
+++ template/base/vimrc.cpp
@@ -0,0 +1,6 @@
+﻿set nu ai ci si mouse=a ts=4 sts=4 sw=4
+nmap<F2> : vs %<.in <CR>
+nmap<F3> : !gedit % <CR>
+nmap<F5> : !./%< < %<.in <CR>
+nmap<F9> : :w <CR> :make %< <CR>
+nmap<F10> : !g++ % -o %< -O2 -g -std=c++11 -Wall <CR>
