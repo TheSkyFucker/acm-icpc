5d26c6e30ce277fbd4064186b67c86c13c44d106
kino <359084415@qq.com>
2018-01-02 22:52:11 +0800

    add template<base/header>.

---
+++ template/base/header.cpp
@@ -0,0 +1,24 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+typedef double db;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+#define rep(i,l,r) for(int i=(l);i<(r);++i)
+#define per(i,l,r) for(int i=(r)-1;i>=(l);--i)
+#define dd(x) cout << #x << " = " << x << ", "
+#define de(x) cout << #x << " = " << x << endl
+
+//-----
+
+int main() {
+    
+    
+    return 0;
+}
