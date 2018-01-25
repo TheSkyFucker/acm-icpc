ed4ff38f79133e6034caeef2598856970c05efd0
kino <359084415@qq.com>
2018-01-25 10:26:33 +0800

    add template<string/base>.

---
+++ template/string/base.cpp
@@ -0,0 +1,4 @@
+//description : search substr
+//time : strlen(sour) * strlen(des)
+char * strstr(char *sour, const char *des);  //case sensitive
+char * stristr(char *sour, const char *des); //case insensitive
