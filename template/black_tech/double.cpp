71fef0e70fd5791429f07ed05808450ae879b608
andwho <359084415@qq.com>
2018-02-03 20:24:31 +0800

    add template<black_tech/double/double2interger>.

--- template/black_tech/double.cpp
+++ template/black_tech/double.cpp
@@ -1,3 +1,15 @@
+
+//double to interger
+int sgn(int x) { return (x > 0) - (x < 0); }
+int lower(int, int);
+int upper(int a,int b) { 
+	return sgn(a) * sgn(b) < 0 ? -lower(a * sgn(a), b * sgn(b)) : a / b + (a % b != 0); 
+}
+int lower(int a,int b) {
+  return sgn(a) * sgn(b) < 0 ? -upper(a * sgn(a), b * sgn(b)) : a / b;
+}
+
+//get gcd
 db gcd(db x, db y) {
 	while (sgn(x) > 0 && sgn(y) > 0) {
 		if (x > y) x -= floor(x / y) * y;
