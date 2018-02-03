2c79fb0526eaaea880e98328bd549c3427c7767a
andwho <359084415@qq.com>
2018-02-03 20:30:18 +0800

    update <template/base/header>.

--- template/base/header.cpp
+++ template/base/header.cpp
@@ -8,6 +8,7 @@ typedef pair<int, int> pii;
 #define se second
 #define mp make_pair
 #define pb push_back
+#define pw(x) (1ll << (x))
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(),(x).end()
 #define rep(i,l,r) for(int i=(l);i<(r);++i)
