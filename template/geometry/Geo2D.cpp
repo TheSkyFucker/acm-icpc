c493fe39228faae275b728ecc4011bea51ea96d4
andwho <359084415@qq.com>
2018-05-03 14:11:51 +0800

    upd template<Geo2D>.

--- template/geometry/Geo2D.cpp
+++ template/geometry/Geo2D.cpp
@@ -3,8 +3,9 @@ T eps = 1e-10;
 int sgn(T d) { return (d > eps) - (d < -eps); }
 struct P {
 	T x, y;
-	P() {}
-	P(T x, T y): x(x), y(y) {}
+	P(){}P(T x, T y): x(x), y(y) {}
+	void read() { scanf("%lf%lf", &x, &y); }
+	void out() { dd(x); de(y); }
 	T operator *(const P &b) const { return x * b.x + y * b.y; }
 	T operator /(const P &b) const { return x * b.y - y * b.x; }
 	P operator +(const P &b) const { return P(x + b.x, y + b.y); }
@@ -13,8 +14,11 @@ struct P {
 	P operator /(const T &d) const { return P(x / d, y / d); }
 	T abs() const { return sqrt(x * x + y * y); }
 	T norm() const { return x * x + y * y; }
+	// (abs(), 0) 逆时针 rot ang() => (x, y)
+	T ang() const { return atan2(y, x); } //[-pi, pi]
+	//逆时针 rot pi/2
 	P rot90() const { return P(-y, x); }
-	void show() { dd(x); de(y); }
-	void read() { scanf("%lf%lf", &x, &y); }
+	//逆时针 rot a
+	P rot(const T &a) const { return P(cos(a) * x - sin(a) * y, sin(a) * x + cos(a) * y); } 
 };
 
