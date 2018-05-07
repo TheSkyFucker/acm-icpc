c8fd1bd200a61eea6231ab5b276d58f3dfbf7972
andwho <359084415@qq.com>
2018-05-07 16:18:40 +0800

    add template<black_tech/io>.

--- template/black_tech/io.cpp
+++ template/black_tech/io.cpp
@@ -2,3 +2,13 @@
 // 在这之后不要混用 cout 和 printf 之类
 std::ios::sync_with_stdio(false);
 std::cin.tie(0);
+
+//cin char, char[]
+cin.ignore(); // ignore a char
+cin.get();    // return -1(EOF)
+cin.get(ch);  // return 0(EOF)
+cin.get(字符数组, 字符个数n, 终止字符)     //读取 n-1 个，终止符保留在读入流中
+cin.getline(字符数组, 字符个数n, 终止字符) //读取 n-1 个，终止符不保留
+
+//cin string
+getline(cin, str, 终止字符) //终止符不保留
