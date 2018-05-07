e1db727669e8bdc70af6f96c2987d49639f51aab
andwho <359084415@qq.com>
2018-05-07 15:37:45 +0800

    upd template<string/base>.

--- template/string/base.cpp
+++ template/string/base.cpp
@@ -1,4 +1,7 @@
-//description : search substr
+//desc : change case ::tolower || ::toupper
+void transform(sour.begin(), sour.end(), des.begin(), ::toupper);
+
+//desc : search substr
 //time : strlen(sour) * strlen(des)
 char * strstr(char *sour, const char *des);  //case sensitive
 char * stristr(char *sour, const char *des); //case insensitive
