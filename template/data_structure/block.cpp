ca0e344a484c8cc5a77898ce15b616937763249f
kino <359084415@qq.com>
2018-06-19 03:03:03 +0800

    upd template<ds/block>.

--- template/data_structure/block.cpp
+++ template/data_structure/block.cpp
@@ -9,13 +9,13 @@ const int BSZ = 700, N = 1e5 + 7;
 int bn, bl[N], br[N], sz[N];
 
 void ini(int n) {
-	bn = (n - 1) / BSZ + 1;
-	rep(i, 0, n) a[i] = 0;
-	rep(b, 0, bn) {
-		bl[b] = b * BSZ;
-		br[b] = min(n, (b + 1) * BSZ);
-		sz[b] = br[b] - bl[b];
-	}
+  bn = (n - 1) / BSZ + 1;
+  rep(i, 0, n) a[i] = 0;
+  rep(b, 0, bn) {
+    bl[b] = b * BSZ;
+    br[b] = min(n, (b + 1) * BSZ);
+    sz[b] = br[b] - bl[b];
+  }
 }
 
 //rebuild block b
@@ -23,28 +23,28 @@ void bud(int b) { }
 
 // change [l, r) in block b : rep(b, 0, bn) chg(b, ql, qr);
 void chg(int b, int l, int r) {
-	int nl = max(l, bl[b]);
-	int nr = min(r, br[b]);
-	if (nl >= nr) return;
-	if (nr - nl == sz[b]) {
-        // chg whole block
-	}
-    else {
-        // mybey bud(b)
-        // chg [nl, nr)
-    }
+  int nl = max(l, bl[b]);
+  int nr = min(r, br[b]);
+  if (nl >= nr) return;
+  if (nr - nl == sz[b]) {
+    // chg whole block
+  }
+  else {
+    // mybey bud(b)
+    // chg [nl, nr)
+  }
 }
 
 // query [l, r) in block b : rep(b, 0, bn) qry(b, ql, qr);
 ? qry(int b, int l, int r) {
-	int nl = max(l, bl[b]);
-	int nr = min(r, br[b]);
-	if (nl >= nr) return 0;
-	if (nr - nl == sz[b]) {
-        // qry whole block
-    }
-    else {
-        bud(b);
-        // qry [nl, nr)
-    }
+  int nl = max(l, bl[b]);
+  int nr = min(r, br[b]);
+  if (nl >= nr) return 0;
+  if (nr - nl == sz[b]) {
+    // qry whole block
+  }
+  else {
+    bud(b);
+    // qry [nl, nr)
+  }
 }
