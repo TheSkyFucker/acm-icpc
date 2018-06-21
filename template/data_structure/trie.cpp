787f1e1cd03c7c3fd9ade5322f0e5f2b47a20af5
kino <359084415@qq.com>
2018-06-21 21:26:33 +0800

    upd template<ds/trie.cpp

--- template/data_structure/trie.cpp
+++ template/data_structure/trie.cpp
@@ -1,16 +1,25 @@
 // 'a' .. 'z'
 // !!!! : ini() 
 struct Trie {
-	static const int N = ::N;
-	int son[N][26], _;
-	int ne() { fill_n(son[_], 26, -1); return _++; }
-	void ini() { _ = 0; ne(); }
-	void insert(const string &s) {
-		int u = 0;
-		for (auto ch : s) {
-			int &v = son[u][ch - 'a'];
-			if (!~v) v = ne();
-			u = v;
-		}
-	}
-}	tree;
+  static const int N = ::N;
+  int son[N][26], _;
+  int ne() { fill_n(son[_], 26, -1); return _++; }
+  void ini() { _ = 0; ne(); }
+  void insert(const string &s) {
+    int u = 0;
+    for (auto ch : s) {
+      int &v = son[u][ch - 'a'];
+      if (!~v) v = ne();
+      u = v;
+    }
+  }
+  void comb(int a, int b) { // a into b
+    //TODO : something like sz[b] += sz[a];
+    rep(i, 0, 26) if (~son[a][i]) {
+      int &as = son[a][i];
+      int &bs = son[b][i];
+      if (!~bs) bs = ne();
+      comb(as, bs);
+    }
+  }
+} tree;
