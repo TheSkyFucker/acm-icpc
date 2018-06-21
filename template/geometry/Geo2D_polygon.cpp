3b33e5548e583773e7a785f75fb5244f35013b08
kino <359084415@qq.com>
2018-06-21 21:19:28 +0800

    add template<geo/polygon.inPpolygin>.

--- template/geometry/Geo2D_polygon.cpp
+++ template/geometry/Geo2D_polygon.cpp
@@ -11,14 +11,27 @@ T area(polygon p) {
 // !!!! : pay attention if the area is zero
 // time : O(sz(P));
 P gravity(polygon p) {
-	P o = p[0], g(0, 0);
-	rep(i, 0, sz(p)) p[i] = p[i] - o;
-	p.pb(p[0]);
-	T area = 0;
-	rep(i, 0, sz(p) - 1) {
-		T tmp = p[i] / p[i + 1];
-		g = g + (p[i] + p[i + 1]) * tmp;
-		area += tmp;
-	}
-	return g / (3. * area) + o;
+  P o = p[0], g(0, 0);
+  rep(i, 0, sz(p)) p[i] = p[i] - o;
+  p.pb(p[0]);
+  T area = 0;
+  rep(i, 0, sz(p) - 1) {
+    T tmp = p[i] / p[i + 1];
+    g = g + (p[i] + p[i + 1]) * tmp;
+    area += tmp;
+  }
+  return g / (3. * area) + o;
+}
+
+// point in polygon
+int inPpolygon(P p, polygon A) { // -1 : on , 0 : out , 1 : in
+  int res = 0;
+  rep(i, 0, sz(A)) {
+    P u = A[i], v = A[(i + 1) % sz(A)];
+      if (onPS(p, u, v)) return -1;
+      T cross = sgn((v - u) / (p - u)), d1 = sgn(u.y - p.y), d2 = sgn(v.y - p.y);
+      if (cross > 0 && d1 <= 0 && d2 > 0) ++ res;
+      if (cross < 0 && d2 <= 0 && d1 > 0) -- res;
+  }
+  return res;
 }
