cda4581cf20dfaa79c476658a7792104579cfa9b
andwho <359084415@qq.com>
2018-06-30 15:26:32 +0800

    upd template<ds/cartesian_tree>.cpp

--- template/data_structure/cartesian_tree.cpp
+++ template/data_structure/cartesian_tree.cpp
@@ -1,19 +1,15 @@
 // desc : bud a cartesion tree from a[0] .. a[n - 1]
 // time : O(N)
-// !!!! : return root of the tree
-int ls[N], rs[N], root;
+// !!!! : return rt, a[n] will be rewrite
+int ls[N], rs[N];
 int cartesion_tree(int a[], int n) {
-	vi stk;
-	fill_n(ls, n, -1);
-	fill_n(rs, n, -1);
+	a[n] = INT_MAX;
+	vi v(1, n);
+	fill_n(ls, n, -1), fill_n(rs, n, -1);
 	rep(i, 0, n) {
-		int pre = -1;
-		while (!stk.empty() && a[stk.back()] < a[i]) {
-			pre = stk.back(); stk.pop_back();
-		}
-		ls[i] = pre;
-		if (!stk.empty()) rs[stk.back()] = i;
-		stk.pb(i);
+		while (a[v.back()] < a[i])
+			ls[i] = v.back(), v.pop_back();
+		v.pb(rs[v.back()] = i);
 	}
-	return stk[0];
+	return v[1];
 }
